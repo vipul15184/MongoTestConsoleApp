28511725951e25e442d2e4a5df77ab88eaf7ee43
bhavesh patel <vipul15184@yahoo.co.in>
2012-08-20 12:29:07 +0530

    first version for saving simple object data in DB

---
+++ MongoTestConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Norm;
+
+namespace MongoTestConsoleApp
+{
+    public class Employee
+    {
+        [MongoIdentifier]
+        public ObjectId Id { get; set; }
+
+        public string Name { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+            emp.Name = "emp1";
+
+            IMongo db = Mongo.Create("mongodb://localhost/MongoTest");
+
+            db.GetCollection<Employee>().Save(emp);
+        }
+    }
+}
